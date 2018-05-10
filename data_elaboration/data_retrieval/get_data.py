693a634cd2bf98bdd9533f0a05be3dc46f3997cd
alessio <alessiomolinar@gmail.com>
2018-05-10 19:26:20 +0200

    modifiche performance e integrità dati

--- data_elaboration/data_retrieval/get_data.py
+++ data_elaboration/data_retrieval/get_data.py
@@ -2,16 +2,21 @@ from typing import Dict
 import requests
 import json
 import time
+from collections import deque
 from data_elaboration.data_retrieval.data_structures import Repo, CustomJsonEncoder
 
 CONTRIBUTORS_ENDPOINT = "https://api.github.com/repos/{}/{}/stats/contributors"
 REPOS_ENDPOINT = "https://api.github.com/users/{}/repos"
 user_dict = dict()
 visited_repos = set()
+queue = deque()
 # For requests using Basic Authentication or OAuth, you can make up to 5000 requests per hour.
 # t_min = 5000/(60*60) = 1.38s
 t = 2
 depth = 2
+
+_loggedDataContributors = list()
+_loggedDataRepos = list()
 
 
 # ottengo lista contributors della repository in analisi e rispettivi commits (passata come argomento)
@@ -25,6 +30,8 @@ def get_repo_contributors(owner, repo):
     if response.status_code == 204:
         return
     data_json = response.json()
+    # TODO rimuovi qui
+    _loggedDataContributors.append(data_json)
 
     visited_repos.add(repo)
     return get_first_five_contributors(data_json)
@@ -52,6 +59,9 @@ def get_user_repos(user):
     response = requests.get(REPOS_ENDPOINT.format(user), auth=('socialNetworkAnalysis', 'XTfLGSS3'))
     print(response)
     repos_json = response.json()
+
+    # TODO RIMUOVI QUI
+    _loggedDataRepos.append(repos_json)
     repo_list = list()
 
     for elem in repos_json:
@@ -72,26 +82,31 @@ def run_from_data(data, repo_name, language):
         # The method setdefault() is similar to get(), but will set dict[key]=default if key is not already in dict.
         repos = user_dict.setdefault(username, [])
         repos.append(Repo(repo_name, user[1], language))
+        queue.append(username)
 
 
 def save_data(data, file):
     json.dump(data, open(file, "w"), cls=CustomJsonEncoder)
 
 
+# Analizza fino a profondità desired_depth le repo di ogni utente nella queue
 def analyze_to_depth(desired_depth):
     for x in range(desired_depth):
-        _user_dict_copy = user_dict.copy()
-        # individuo in user_dict gli utenti di partenza dell'analisi
-        for user in _user_dict_copy:
+        while len(queue) > 0:
+            user = queue.popleft()
             print("Analyzing user: {}".format(user))
-            # per l'utente in analisi, ottengo lista delle sue repository
-            repos_data = get_user_repos(user)
-            for repo in repos_data:
-                print("Analyzing repo: {}".format(repo[0]))
-                # per la repo in analisi, ottengo lista contributors
-                contributors_data = get_repo_contributors(user, repo[0])
-                # chiamo funzione run_from_data che inserisce utenti/oggetti Repo in user_dict
-                run_from_data(contributors_data, repo[0], repo[1])
+            collect_data_on_user_repos(user)
+
+
+def collect_data_on_user_repos(user):
+    # per l'utente in analisi, ottengo lista delle sue repository
+    repos_data = get_user_repos(user)
+    for repo in repos_data:
+        print("Analyzing repo: {}".format(repo[0]))
+        # per la repo in analisi, ottengo lista contributors
+        contributors_data = get_repo_contributors(user, repo[0])
+        # chiamo funzione run_from_data che inserisce utenti/oggetti Repo in user_dict
+        run_from_data(contributors_data, repo[0], repo[1])
 
 
 # funzione di test per analisi errori su specifici user/nodi
