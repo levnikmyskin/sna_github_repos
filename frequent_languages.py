95039f7cca23e084088f522cc489525bedc9553e
alessio <alessiomolinar@gmail.com>
2018-05-13 15:18:07 +0200

    aggiunti script utility

---
+++ frequent_languages.py
@@ -0,0 +1,15 @@
+import json
+import collections
+
+d = json.load(open("data.json", "r"))
+
+language_list = list()
+
+for _, repo_list in d.items():
+    for repo in repo_list:
+        for _, repoinfo in repo.items():
+            language_list.append(repoinfo["language"])
+
+c = collections.Counter(language_list)
+print(c.most_common())
+
