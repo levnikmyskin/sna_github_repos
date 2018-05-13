353c472d4f4cba0bfcb6313dd96c7365bf0ec5e1
alessio <alessiomolinar@gmail.com>
2018-05-13 15:08:40 +0200

    aggiunto script controllo

---
+++ controllo_depth.py
@@ -0,0 +1,11 @@
+import json
+
+data = json.load(open("depth3.json", "r"))
+
+for user, repo_list in data.items():
+    visited_repo = list()
+    for repo in repo_list:
+        if repo in visited_repo:
+            print("MA CHE CAZZ", repo)
+            quit()
+        visited_repo.append(repo)
