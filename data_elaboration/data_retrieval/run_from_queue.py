63296ef04cbc3f5f78e80995eb6c8a4140035a1d
alessio <alessiomolinar@gmail.com>
2018-05-16 00:30:34 +0200

    nuovi dati

--- data_elaboration/data_retrieval/run_from_queue.py
+++ data_elaboration/data_retrieval/run_from_queue.py
@@ -4,21 +4,14 @@ import traceback
 import signal
 from queue2135 import queue
 
-def any_signal():
-    save_data(user_dict, "depth4.json")
 
 user_dict = json.load(open("depth3.json", 'r'))
-for i in [s for s in dir(signal) if s.startswith("SIG")]:
-    try:
-        signum = getattr(signal, i)
-        signal.signal(signum, any_signal)
-    except:
-        print("Skipping ", i)
+
 try:
     save_user_queue_contributors(queue, user_dict)
     save_user_queue_contributors(queue, user_dict)
 except:
-   print("Abnormal interruption, saving data")
-   print(traceback.format_exc())
+    print("Abnormal interruption, saving data")
+    print(traceback.format_exc())
 finally:
     save_data(user_dict, "depth4.json")
