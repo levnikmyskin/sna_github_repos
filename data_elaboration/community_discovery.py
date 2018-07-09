d4396d9d2a956b36b6d2beea93cac4d2d18f6ee5
VpsVmi <vps@vmi.com>
2018-07-09 19:40:42 +0200

    asdf

--- data_elaboration/community_discovery.py
+++ data_elaboration/community_discovery.py
@@ -1,9 +1,28 @@
+import networkx as nx
 from networkx.algorithms import community
 from networkx.algorithms.community import k_clique_communities
 from community import community_louvain
 import demon as d
 import pquality
-from nf1 import NF1
+
+# TODO: tutto questo schifo d'importazione da sistemare
+
+# TODO: AH NO. Dobbiamo fare Community Evaluation SOLAMENTE SUL CRAWLED DATA top
+
+# TODO: far sì che qui vengano importati le reti impostate in network_setup. Per velocità adesso utilizzo
+# delle reti generate random
+graph_ba = nx.barabasi_albert_graph(100, 5, seed=None)
+graph_crawled = "QUA_IMPORTARE_L_CRWALED_GRAPH"
+
+
+# def get_girvan_newman(graph, iteration):
+#     girvan_list = list()
+#     comm_generator = community.girvan_newman(graph)
+#     limited = itertools.takewhile(lambda t: len(t) <= iteration, comm_generator)
+#     for communities in limited:
+#         girvan_list.append(tuple(sorted(c) for c in communities))
+#
+#     return girvan_list
 
 
 def get_girvan_newman(graph, iteration):
@@ -20,7 +39,7 @@ def get_k_clique(graph, k):
 
     return clique
 
-# TODO: capire differenza fra due modelli label_prop
+
 def get_label_prop(graph):
     # comm_generator = community.label_propagation_communities(graph)
     comm_generator = community.asyn_lpa_communities(graph)
@@ -28,8 +47,9 @@ def get_label_prop(graph):
 
     return comm_generator
 
+
 def get_louvain(graph):
-    partition = community_louvain.best_partition(graph, weight="weight")
+    partition = community_louvain.best_partition(graph)
 
     list_nodes = list()
     for community in set(partition.values()):
@@ -44,7 +64,7 @@ def get_louvain(graph):
 
 def get_demon(graph):
     dm = d.Demon(graph=graph, epsilon=0.25, min_community_size=3)
-    # TODO: ma questa loading bar vorrei toglierla ma non ho voglia di cercare come...
+    # TODO: boh ma che cazzo è sta barra io non la voglio lol
     communities = dm.execute()
 
     return communities
@@ -63,69 +83,39 @@ def get_partition_quality(graph, partition):
     print(results["Indexes"])
 
 
-def run_community_discovery_task(graph):
-    clique_part = get_k_clique(graph, 3)
-    girv_part = get_girvan_newman(graph, 10)
-    label_part = get_label_prop(graph)
-    louv_part = get_louvain(graph)
-    demon_part = get_demon(graph)
+def main():
+    clique_part = get_k_clique(graph_ba, 3)
+    girv_part = get_girvan_newman(graph_ba, 10)
+    label_part = get_label_prop(graph_ba)
+    louv_part = get_louvain(graph_ba)
+    demon_part = get_demon(graph_ba)
 
     print_results(clique_part, girv_part, label_part, louv_part, demon_part)
-    run_pquality_test(graph, clique_part, girv_part, label_part, louv_part, demon_part)
 
-    partition_list = list(((clique_part, "Clique_part"), (girv_part, "Girv_part"), (label_part, "Label_part"),
-                           (louv_part, "Louv_part"), (demon_part, "Demon_part")))
-
-    run_nf1_evaluation(partition_list)
-
-    return partition_list
-
-
-def run_nf1_evaluation(partition_list):
-    for partition in partition_list:
-        _partition_list_copy = partition_list.copy()
-        _partition_list_copy.remove(partition)
-        index = partition_list.index([partition][0])
-
-        for comparable_partition in _partition_list_copy[index:]:
-            nf = NF1(partition[0], comparable_partition[0])
-            res = nf.summary()
-
-            print(f"Comparison between {partition[1]} and {comparable_partition[1]}:")
-            print(res["scores"])
-            print("____________________________________________________")
-
-            # print("Details:")
-            # print("\n")
-            # print(res["details"])
-            # print("____________________________________________________")
-            # print("\n")
-
-
-def run_pquality_test(graph, clique_part, girv_part, label_part, louv_part, demon_part):
     print("\n")
     print("Clique Evaluation")
-    get_partition_quality(graph, clique_part)
+    get_partition_quality(graph_ba, clique_part)
     print("____________________________________________________________")
 
     print("\n")
     print("Girvan-Newman Evaluation")
-    get_partition_quality(graph, girv_part)
+    get_partition_quality(graph_ba, girv_part)
     print("____________________________________________________________")
 
     print("\n")
     print("Label Propagation Evaluation")
-    get_partition_quality(graph, label_part)
+    get_partition_quality(graph_ba, label_part)
     print("____________________________________________________________")
 
     print("\n")
     print("Louvain Evaluation")
-    get_partition_quality(graph, louv_part)
+    get_partition_quality(graph_ba, louv_part)
     print("____________________________________________________________")
 
     print("\n")
     print("Demon Evaluation")
-    get_partition_quality(graph, demon_part)
+    get_partition_quality(graph_ba, demon_part)
     print("____________________________________________________________")
 
 
+main()
